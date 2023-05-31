76065b2ee52a5db6f934ce658351bbfa75762bbb
Emilee Hatch <121782677+EmToHat@users.noreply.github.com>
2023-05-30 18:33:23 -0600

    Add files via upload

---
+++ wdd230/chamber/js/directory.js
@@ -0,0 +1,52 @@
+const requestURL = 'https://emt0rres.github.io/wdd230/chamber/data/directory.json';
+
+const cards = document.querySelector('div.businessCards');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const companies = jsonObject['companies'];
+    companies.forEach(displayCompanies);
+  });
+
+function displayCompanies(company) {
+    
+  // Create elements to add to the document
+    let card = document.createElement('section');
+    let companyLogo = document.createElement('img');
+    let companyName = document.createElement('h2');
+    let companyAddress = document.createElement('p');
+    let companyPhone = document.createElement('p');
+    let companyWebsite = document.createElement('a');
+    
+    companyName.innerHTML = `${company.name}`;
+    companyAddress.innerHTML = `${company.address}`;
+    companyPhone.innerHTML = `${company.phone}`;
+    companyWebsite.innerHTML = `${company.website}`;
+
+    companyLogo.setAttribute('src', company.imageurl);
+    companyLogo.setAttribute('alt', 'Logo of ' + `${company.name}`);
+    companyLogo.setAttribute('loading', 'lazy');
+    companyWebsite.setAttribute('href', `${company.website}`)
+    
+    card.appendChild(companyLogo);
+    card.appendChild(companyName);
+    card.appendChild(companyAddress);
+    card.appendChild(companyPhone);
+    card.appendChild(companyWebsite);
+    
+    cards.appendChild(card);
+}
+
+const gridView = document.querySelector('.gridViewbtn');
+// const selectView = document.querySelector('.cards');
+const listView = document.querySelector('.listViewbtn');
+
+gridView.addEventListener('click', () => {cards.classList.add('businessCards')}, once="true");
+gridView.addEventListener('click', () => {cards.classList.remove('switchListView')}, once="true");
+
+listView.addEventListener('click', () => {cards.classList.add('switchListView')}, once="true");
+listView.addEventListener('click', () => {cards.classList.remove('businessCards')}, once="true");
---
+++ wdd230/chamber/js/index.js
@@ -0,0 +1,86 @@
+// _____Lazy Load Images____
+// variable to get all imgs w/data-src  attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// arrow function which processes the data-src attributes
+// function expression
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
+
+// if this works on the browser
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+  // Loads images
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+/*__________________________________*/
+
+// _____Updates____
+function updates() {
+  let lastUpdate = document.lastModified;
+  document.getElementById("modified").innerHTML = lastUpdate;
+
+  const date = new Date();
+  const year = date.getFullYear();
+  document.getElementById("year").innerHTML = year;
+}
+/*__________________________________*/
+
+// _____Hamburger Menu____
+function toggleMenu() {
+  document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
+}
+/*__________________________________*/
+
+// _____Greeting Banner____
+//const now = new Date()
+//date = new Intl.DateTimeFormat().format(now)
+
+function dayOfweek() {
+  let message;
+
+  let currentDate = new Date();
+  weekdayValue = currentDate.getDay();
+
+  if (weekdayValue == 2) {
+    message = "📢 Join us for the chamber meet & greet Wednesday at 7:00 p.m.";
+    document.getElementById("greeting").innerHTML = message;
+  } else {
+    /*else if (weekdayValue == 3)
+    {
+        message = "Come join us for the chamber meet and greet Tonight at 7:00 p.m.";
+        document.getElementById("greeting").innerHTML = message;
+    }*/
+    document.getElementById("greeting").style.display = "none";
+  }
+}
+/*__________________________________*/
+
+
+//Output
+updates();
+dayOfweek();
